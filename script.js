1a2161bbd6e972dc55729f0bbafdc5eb9df33164
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-17 01:08:12 -0600

    started pseudo in js

---
+++ script.js
@@ -0,0 +1,20 @@
+// api key = 4c74f6b3863580eb98284d7f219339f3
+
+// id's used
+  // submitbtn: submit button for city search
+
+
+// class's used on weather elements
+  // weather-search : city search input area
+  // weather: container for  current weather
+  // forecast: container for 5 day weather cards
+  // card: forecast card
+  // city
+  // date
+  // temp
+  // min-temp
+  // max-temp
+  // feels-like
+  // humidity
+  // rain
+
