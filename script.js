532a93ad692613201063f3d7b0aefa03646b8ac2
Jeff Hammer <jeffrey.hammer2019@gmail.com>
2023-02-17 22:20:49 -0600

    more pseudo done moving on to something else for now

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// api key = 4c74f6b3863580eb98284d7f219339f3
-
 // id's used
   // submitbtn: submit button for city search
 
@@ -18,3 +16,16 @@
   // humidity
   // rain
 
+var APIKEY = "4c74f6b3863580eb98284d7f219339f3";
+var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q={city}&units={imperial}&appid={API key}"
+var city;
+
+// TODO write fetch
+
+// form submit handler
+
+// search button click event handler
+
+// store searched city in local storage
+
+// append weather data to html elements
